7c17f71af4a3b507fdb838d15734f9cc19d79e83
Sidrit Vejseli <vjs5009a@ensfsi.onmicrosoft.com>
2025-03-28 15:29:49 +0100

    test

---
+++ test/controleur/ControlAcheterProduitTest.java
@@ -0,0 +1,24 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControlAcheterProduitTest {
+
+	@Test
+	void testControlAcheterProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCotrolEtatMarche() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAcheterProduit() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/controleur/ControlAfficherVillageTest.java
@@ -0,0 +1,56 @@
+package controleur;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlAfficherVillageTest {
+	
+	private Village village;
+	private Chef abraracourcix;
+	private ControlAfficherVillage controlAfficherVillage;
+	private ControlEmmenager controlEmmenager;
+
+	@BeforeEach
+	public void initialiserSituation() {
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+		controlAfficherVillage = new ControlAfficherVillage(village);
+		controlEmmenager = new ControlEmmenager(village);
+	}
+	
+	
+	@Test
+	void testControlAfficherVillage() {
+		assertNotNull(controlAfficherVillage, "Constructeur ne renvoie pas null");	}
+
+	@Test
+	void testDonnerNomsVillageois() {
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		controlEmmenager.ajouterGaulois("Abraracourcix", 10);
+		assertTrue(Arrays.stream(controlAfficherVillage.donnerNomsVillageois()).anyMatch("Bonemine"::equals));
+		assertFalse(Arrays.stream(controlAfficherVillage.donnerNomsVillageois()).anyMatch("Pablo"::equals));
+		assertTrue(Arrays.stream(controlAfficherVillage.donnerNomsVillageois()).anyMatch("Abraracourcix"::equals));
+		
+
+		
+	}
+
+	@Test
+	void testDonnerNomVillage() {
+		assertTrue(controlAfficherVillage.donnerNomVillage().equals("le village des irréductibles"));	
+				}
+
+	@Test
+	void testDonnerNbEtals() {
+		fail("Not yet implemented");
+	}
+
+}
